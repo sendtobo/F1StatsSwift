1944371fdbe679ad22555b0a2bfdc2cacc1b803e
blaine.oelkers@vivino.com <blaine.oelkers@vivino.com>
2023-02-19 14:56:37 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# F1StatsSwift
+
+A description of this package.
---
+++ Tests/F1StatsSwiftTests/F1StatsSwiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import F1StatsSwift
+
+final class F1StatsSwiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(F1StatsSwift().text, "Hello, World!")
+    }
+}
